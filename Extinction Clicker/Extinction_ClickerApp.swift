0f184d9c4ee78297e883049841ccc894df2d7367
DrNogNog <gordoncs@outlook.com>
2024-04-25 20:09:04 -0400

    Initial Commit

---
+++ Extinction Clicker/Extinction_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Extinction_ClickerApp.swift
+//  Extinction Clicker
+//
+//  Created by Gordon on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Extinction_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
